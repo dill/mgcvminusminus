468f66485d55134e5fdda3f6a5ef84a5d4ad625b
David Lawrence Miller <github@ninepointeightone.net>
2019-09-28 16:27:51 -0400

    this code compiles?

--- inst/gam_examples.R
+++ inst/gam_examples.R
@@ -1,23 +1,6 @@
-# mgcvminusminus
+# examples from mgcv::gam
+library(mgcv)
 
-<!-- badges: start -->
-<!-- badges: end -->
-
-`mgcvminusminus` autogenerates TMB code from `mgcv` model specifications.
-
-## Installation
-
-You can install the released version of `mgcvminusminus` via
-
-``` r
-remotes::install_github("dill/mgcvminusminus")
-```
-
-## Example
-
-``` r
-library(mgcvminusminus)
-## basic example code
 set.seed(2) ## simulate some data... 
 dat <- gamSim(1,n=400,dist="normal",scale=2)
 #b <- gam(y~s(x0)+s(x1)+s(x2)+s(x3),data=dat)
@@ -41,5 +24,8 @@ model <- MakeADFun(data=tmb$jags.data, parameters=tmb$jags.ini,
 # actually do the optimisation
 # lots of output from the trace
 opt <- nlminb(model$par, model$fn, model$gr, control=list(trace=1))
-```
+
+# calculate standard deviations for the estimated pars
+rep <- sdreport(model)
+
 
