7692402003bb90435d9954b44b18fbda4170704e
David Lawrence Miller <github@ninepointeightone.net>
2019-10-09 21:49:13 +0100

    example update

--- inst/gam_examples.R
+++ inst/gam_examples.R
@@ -3,13 +3,12 @@ library(mgcv)
 
 set.seed(2) ## simulate some data... 
 dat <- gamSim(1,n=400,dist="normal",scale=2)
-#b <- gam(y~s(x0)+s(x1)+s(x2)+s(x3),data=dat)
-
 
 source("../R/TMBam.R")
+source("../R/lp.R")
 
-#tmb <- TMBam(y~s(x0)+s(x1)+s(x2)+s(x3),data=dat, file="thingo")
-tmb <- TMBam(y~s(x0),data=dat, file="thingo")
+tmb <- TMBam(y~s(x0)+s(x1)+s(x2)+s(x3),data=dat, file="thingo")
+#tmb <- TMBam(y~s(x0),data=dat, file="thingo")
 
 library(TMB)
 compile("thingo.cpp")
@@ -29,3 +28,5 @@ opt <- nlminb(model$par, model$fn, model$gr, control=list(trace=1))
 rep <- sdreport(model)
 
 
+#bb <- gam(y~s(x0),data=dat, select=TRUE, method="REML")
+bb <- gam(y~s(x0)+s(x1)+s(x2)+s(x3),data=dat, select=TRUE, method="REML")
